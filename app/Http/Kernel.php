54327533632e68296c844ddc1d446b86e11a8ab4
Nick Hamilton <nicholashamilton.co@gmail.com>
2023-06-05 13:17:38 -0400

    Update middlewareAliases to use v10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
